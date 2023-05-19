1d8e2e2dba4cd0b0d60e6036794f6a4a641dbabb
AgustinTrigo(re) <a_trigo295@hotmail.com>
2023-05-19 15:26:15 -0300

    Config-navbar-display-menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-let navbarBtn = document.querySelector(".navbar-btn")
-let navbarMenu = document.querySelector(".navbar")
+let navbarBtn = document.querySelector(".navbar-btn");
+let navbarMenu = document.querySelector(".navbar");
+let navbarMenuList = document.createElement("ul");
+navbarMenuList.setAttribute("id","menu")
+navbarMenuList.className = "navbar-menu";
 
 navbarBtn.addEventListener("click", ()=>{
+    let estadoMenu = navbarMenu.getAttribute("class");
     navbarBtn.classList.toggle("active");
     navbarMenu.classList.toggle("display-menu");
+    navbarMenu.appendChild(navbarMenuList);
+    navbarMenuList.innerHTML = `
+    <li>Inicio</li>
+    <li>Playlist</li>
+    <li>Informacion</li>
+    `
+    if(estadoMenu === "navbar display-menu"){
+        navbarMenu.removeChild(navbarMenuList);
+    }
 })
+
