fc839555d47e9d823c80f4457af170a48ecfb6bc
AgustinTrigo <a_trigo295@hotmail.com>
2023-05-19 00:12:52 -0300

    Config-navbar-display(c)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let navbarBtn = document.querySelector(".navbar-btn")
+let navbarMenu = document.querySelector(".navbar")
 
-
-navbarBtn.addEventListener("click", ()=>{navbarBtn.classList.toggle("active")})
+navbarBtn.addEventListener("click", ()=>{
+    navbarBtn.classList.toggle("active");
+    navbarMenu.classList.toggle("display-menu");
+})
